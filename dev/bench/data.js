b2d7310ca681aefed85316113c63fcdbd64f2ca3
github-action-benchmark <github@users.noreply.github.com>
2024-06-11 09:23:37 +0000

    add Benchmark (benchmarkjs) benchmark result for cc2f9c58975f72907576c529c2094b330d8b2b92

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718095030255,
+  "lastUpdate": 1718097817089,
   "repoUrl": "https://github.com/ashnashahgrover/cacti",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,37 @@ window.BENCHMARK_DATA = {
             "range": "±1.24%",
             "unit": "ops/sec",
             "extra": "181 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vramakr2@in.ibm.com",
+            "name": "VRamakrishna",
+            "username": "VRamakrishna"
+          },
+          "committer": {
+            "email": "sandeepn.official@gmail.com",
+            "name": "Sandeep Nishad",
+            "username": "sandeepnRES"
+          },
+          "distinct": true,
+          "id": "cc2f9c58975f72907576c529c2094b330d8b2b92",
+          "message": "ci(ossf): configured GitHub action and badge for OpenSSF scorecard\n\nSigned-off-by: VRamakrishna <vramakr2@in.ibm.com>",
+          "timestamp": "2024-06-10T01:11:43+05:30",
+          "tree_id": "8f80768252cab6792094c2872aebb5438315caff",
+          "url": "https://github.com/ashnashahgrover/cacti/commit/cc2f9c58975f72907576c529c2094b330d8b2b92"
+        },
+        "date": 1718097815139,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
+            "value": 715,
+            "range": "±3.16%",
+            "unit": "ops/sec",
+            "extra": "176 samples"
           }
         ]
       }
