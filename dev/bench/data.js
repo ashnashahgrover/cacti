6cb0eb8d27493df390e4d97762a2f6759c5474de
github-action-benchmark <github@users.noreply.github.com>
2024-08-11 21:13:25 +0000

    add Benchmark (benchmarkjs) benchmark result for d0e4539a9b106fa684cd34a6cdb1ff835b870ce4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722819640496,
+  "lastUpdate": 1723410805018,
   "repoUrl": "https://github.com/ashnashahgrover/cacti",
   "entries": {
     "Benchmark": [
@@ -69,6 +69,44 @@ window.BENCHMARK_DATA = {
             "range": "±3.48%",
             "unit": "ops/sec",
             "extra": "178 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "peter.somogyvari@accenture.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "committer": {
+            "email": "sandeepn.official@gmail.com",
+            "name": "Sandeep Nishad",
+            "username": "sandeepnRES"
+          },
+          "distinct": true,
+          "id": "d0e4539a9b106fa684cd34a6cdb1ff835b870ce4",
+          "message": "ci(github): upgrade actions/github-script to 7.0.1 project-wide\n\nFixes #3458\n\nSigned-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>",
+          "timestamp": "2024-08-09T05:57:27+05:30",
+          "tree_id": "1b12638b0ee30d8845ca2446fe5a82b172922a85",
+          "url": "https://github.com/ashnashahgrover/cacti/commit/d0e4539a9b106fa684cd34a6cdb1ff835b870ce4"
+        },
+        "date": 1723410802278,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 594,
+            "range": "±1.57%",
+            "unit": "ops/sec",
+            "extra": "178 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 348,
+            "range": "±2.04%",
+            "unit": "ops/sec",
+            "extra": "182 samples"
           }
         ]
       }
