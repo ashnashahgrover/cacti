354a17a6a82f5980c3cf48130054ad1bdf6e24b2
github-action-benchmark <github@users.noreply.github.com>
2024-08-21 22:39:23 +0000

    add Benchmark (benchmarkjs) benchmark result for ec9683d38670fe5d657b602db8215e602fd4209d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724279184201,
+  "lastUpdate": 1724279962868,
   "repoUrl": "https://github.com/ashnashahgrover/cacti",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,37 @@ window.BENCHMARK_DATA = {
             "range": "±1.95%",
             "unit": "ops/sec",
             "extra": "181 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "adrian.batuto@accenture.com",
+            "name": "adrianbatuto",
+            "username": "adrianbatuto"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "ec9683d38670fe5d657b602db8215e602fd4209d",
+          "message": "feat(corda): support 5.1 via TS/HTTP (no JVM)\nFixes #2978\nFixes #3293\n\nSigned-off-by: adrianbatuto <adrian.batuto@accenture.com>",
+          "timestamp": "2024-08-19T14:43:10-07:00",
+          "tree_id": "530c66f1928ba9481fcc2d1d760582bf58be6677",
+          "url": "https://github.com/ashnashahgrover/cacti/commit/ec9683d38670fe5d657b602db8215e602fd4209d"
+        },
+        "date": 1724279960184,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
+            "value": 724,
+            "range": "±2.71%",
+            "unit": "ops/sec",
+            "extra": "178 samples"
           }
         ]
       }
